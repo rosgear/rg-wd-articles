db728c306bbdfa81bc356c34dd0e82365562cb36
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-13 15:14:31 +0300

    Файл локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения RosGear.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'Articles',
+    '{description}' => 'Site articles'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения RosGear.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'Список материалов',
+    '{description}' => 'Список материалов сайта',
+
+    // Widget: разметка
+    'Markup settings' => 'Настройка разметки',
+
+    // MarkupSettings
+    '{markupsettings.title}' => 'Настройка разметки виджета "Список материалов"',
+    // MarkupSettings: поля
+    'The markup is done in the template' => 'Разметка в шаблоне',
+    'In the specified template, the widget parameters are changed. You can make changes manually by opening the template for editing.' 
+        => 'В указанном шаблоне изменяются параметры виджета. Изменения вы можете сделать вручную, открыв на редактирование шаблон.',
+    'Work mode' => 'Режим работы',
+    'The operating mode determines whether list control parameters will be accepted from the request' 
+        => 'Режим работы определяет, будут ли приниматься из запроса параметры управления списком',
+    'Full' => 'Полный',
+    'List' => 'Список',
+    'Items' => 'Элементы',
+    'Pagination' => 'Пагинация',
+    'Page parameter' => 'Параметр страницы',
+    'Parameter indicating transition to page' => 'Параметр указывающий переход на страницу',
+    'Limit parameter' => 'Параметр количества',
+    'Parameter indicating the number of elements on the page' => 'Параметр указывающий количество элементов на странице',
+    'Elements per page' => 'Элементов на странице',
+    'Number of elements displayed on the page' => 'Количество элементов отображаемых на странице',
+    'Max. elements per page' => 'Макс. элементов на странице',
+    'Maximum number of elements displayed on the page' => 'Максимальное количество элементов отображаемых на странице',
+    'Limit filter' => 'Фильтр количества',
+    'The number of elements on the page that can be specified is listed through ","' 
+        => 'Через "," перечисляется количество элементов на странице, которое можно указать',
+    'Sorting' => 'Сортировка',
+    'Sort parameter' => 'Параметр сортировки',
+    'By default' => 'По умолчанию',
+    'Default list sorting' => 'Сортировка списка по умолчанию',
+];
