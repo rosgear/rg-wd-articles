f920c126579e74edf077d3ecdaeb05112b464b2b
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-13 15:15:10 +0300

    Installer

---
+++ src/Installer/Installer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения RosGear.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+namespace Rg\Widget\Articles\Installer;
+
+use Ge\Panel\Widget\WidgetInstaller;
+
+/**
+ * Установщик виджета.
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Rg\Widget\Articles\Installer
+ * @since 1.0
+ */
+class Installer extends WidgetInstaller
+{
+}
