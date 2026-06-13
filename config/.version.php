fb4a86b4b9336ca4005baae4a0e3344357026ed8
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-13 15:15:41 +0300

    Файл конфигурации версии виджета

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения RosGear.
+ * 
+ * Файл конфигурации версии виджета.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'name'        => 'Articles',
+    'description' => 'List articles',
+    'version'     => '1.0',
+    'versionDate' => '20-12-2017',
+    'author'      => 'RosGear',
+    'authorUrl'   => 'https://rosgear.ru/',
+    'email'       => 'info@rosgear.ru',
+    'url'         => 'https://rosgear.ru/component/rg-wd-articles/',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://rosgear.ru/license/#license-mit'
+];
